4d123b4cb7862b1de88ca0d02bdf7dfcfec5ed31
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-03-15 13:35:04 +0700

    master

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    String test(HttpServletResponse response)
+    {
+        response.setHeader("Access-Control-Allow-Origin","https://code.junookyo.xyz/");
+        response.setHeader("Access-Control-Allow-Headers","origin, x-requested-with, content-type");
+        response.setHeader("Access-Control-Allow-Methods","PUT, GET, POST, DELETE, OPTIONS");
+        return "Ok";
+    }
+}
