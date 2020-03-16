b716dcb1fd286461069bab2067fdbdc3ffe4f609
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-03-16 16:39:28 +0700

    Update Rest.java

--- src/main/java/com/example/demo/Rest.java
+++ src/main/java/com/example/demo/Rest.java
@@ -24,16 +24,41 @@ public class Rest {
         JsonNode node=mapper.readTree(re);
         String out="----TG----\\n" +
                 "Số ca nhiễm : "+node.get("data").get("global").get("cases").asText()+
-                "\\n Số ca tử vong: "+node.get("data").get("global").get("deaths").asText()+
-                "\\n Số ca chữa khỏi: "+node.get("data").get("global").get("recovered").asText()+
+                "\\nSố ca tử vong: "+node.get("data").get("global").get("deaths").asText()+
+                "\\nSố ca chữa khỏi: "+node.get("data").get("global").get("recovered").asText()+
                 "\\n----VN----\\n" +
                 "Số ca nhiễm : "+node.get("data").get("vietnam").get("cases").asText()+
-                "\\n Số ca tử vong: "+node.get("data").get("vietnam").get("deaths").asText()+
-                "\\n Số ca chữa khỏi: "+node.get("data").get("vietnam").get("recovered").asText();
-        return "    {\n" +
-                "        \"messages\": [\n" +
-                "            {\"text\": \""+out+"\"}\n" +
-                "        ]\n" +
-                "    }";
+                "\\nSố ca tử vong: "+node.get("data").get("vietnam").get("deaths").asText()+
+                "\\nSố ca chữa khỏi: "+node.get("data").get("vietnam").get("recovered").asText();
+        return "{\n" +
+                "  \"messages\": [\n" +
+                "    {\n" +
+                "      \"attachment\": {\n" +
+                "        \"type\": \"template\",\n" +
+                "        \"payload\": {\n" +
+                "          \"template_type\": \"button\",\n" +
+                "          \"text\": \"Hello!\",\n" +
+                "          \"buttons\": [\n" +
+                "            {\n" +
+                "              \"type\": \"show_block\",\n" +
+                "              \"block_names\": [\"name of block\"],\n" +
+                "              \"title\": \"Show Block\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"type\": \"web_url\",\n" +
+                "              \"url\": \"https://rockets.chatfuel.com\",\n" +
+                "              \"title\": \"Visit Website\"\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"url\": \"https://rockets.chatfuel.com/api/welcome\",\n" +
+                "              \"type\":\"json_plugin_url\",\n" +
+                "              \"title\":\"Postback\"\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
     }
 }
